2863e8fa1f0a5b28b039f2f59d169ef9ec472dd8
gordonpm <gordonm0815@gmail.com>
2022-09-10 20:01:59 -0700

    Enabled config client which can connect to config server

--- src/main/java/io/gordonpm/springbootconfig/GreetingController.java
+++ src/main/java/io/gordonpm/springbootconfig/GreetingController.java
@@ -2,6 +2,7 @@ package io.gordonpm.springbootconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class GreetingController {
 
     @Value("${my.greeting: defaultValue}")
@@ -25,6 +27,6 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting() {
-        return dbSettings.getConnection() + " " + dbSettings.getHost() + " " + dbSettings.getPort();
+        return "my.greeting=" + greetingMessage + "db.connnection=" + dbSettings.getConnection() + " " + dbSettings.getHost() + " " + dbSettings.getPort();
     }
 }
